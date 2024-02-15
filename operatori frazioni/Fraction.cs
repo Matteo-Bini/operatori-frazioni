4ec3cd3b1c3a4723b83fcabead676f02b0f57a44
matte <matte@DESKTOP-NA3JRQD>
2024-02-15 23:21:00 +0100

    Aggiungere i file di progetto.

---
+++ operatori frazioni/Fraction.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+namespace Frazioni
+{
+    public class Fraction
+    {
+        public int N { get; }
+        public int D { get; }
+
+        public Fraction(int n, int d)
+        {
+            Semplifica(ref n, ref d);
+
+            N = n;
+            D = d;
+        }
+
+        private void Semplifica(ref int n, ref int d)
+        {
+            int mcd = CalcolaMcd(n, d);
+            n = n / mcd;
+            d = d / mcd;
+        }
+
+        private int CalcolaMcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int resto = a % b;
+                a = b;
+                b = resto;
+            }
+            return a;
+        }
+
+        public Fraction Somma(Fraction other)
+        {
+            return new Fraction(this.N * other.D + this.D * other.N, this.D * other.D);
+        }
+
+        public Fraction Sottrai(Fraction other)
+        {
+            return new Fraction(this.N * other.D - this.D * other.N, this.D * other.D);
+        }
+
+        public Fraction Moltiplica(Fraction other)
+        {
+            return new Fraction(this.N * other.N, this.D * other.D);
+        }
+
+        public Fraction Dividi(Fraction other)
+        {
+            return new Fraction(this.N * other.D, this.D * other.N);
+        }
+
+
+        public override string ToString()
+        {
+            return $"{N}/{D}";
+        }
+
+
+        public static Fraction Parse(string s)
+        {
+            string[] parts = s.Split('/');
+            return new Fraction(int.Parse(parts[0]), int.Parse(parts[1]));
+        }
+
+        public static bool operator ==(Fraction f1, Fraction f2)
+        {
+            if (object.ReferenceEquals(f1, null))
+                return object.ReferenceEquals(f2, null);
+            else if (object.ReferenceEquals(f2, null))
+                return false;
+            else
+                return f1.N == f2.N && f1.D == f2.D;
+        }
+        public static bool operator !=(Fraction f1, Fraction f2)
+        {
+            if (object.ReferenceEquals(f1, null))
+                return object.ReferenceEquals(f2, null);
+            else if (object.ReferenceEquals(f2, null))
+                return f1.N != f2.N && f1.D != f2.D;
+            else
+                return false;
+        }
+        public static bool operator >=(Fraction f1, Fraction f2)
+        {
+            if (object.ReferenceEquals(f1, null))
+                return object.ReferenceEquals(f2, null);
+            else if (object.ReferenceEquals(f2, null))
+                return true;
+            else
+                return f1.N < f2.N && f1.D < f2.D;
+                
+        }
+        public static bool operator >(Fraction f1, Fraction f2)
+        {
+            if (object.ReferenceEquals(f1, null))
+                return object.ReferenceEquals(f2, null);
+            else if (object.ReferenceEquals(f2, null))
+                return true;
+            else
+                return f1.N <= f2.N && f1.D <= f2.D;
+        }
+        public static bool operator <=(Fraction f1, Fraction f2)
+        {
+            if (object.ReferenceEquals(f1, null))
+                return object.ReferenceEquals(f2, null);
+            else if (object.ReferenceEquals(f2, null))
+                return false;
+            else
+                return f1.N <= f2.N && f1.D <=f2.D;
+        }
+        public static bool operator <(Fraction f1, Fraction f2)
+        {
+            if (object.ReferenceEquals(f1, null))
+                return object.ReferenceEquals(f2, null);
+            else if (object.ReferenceEquals(f2, null))
+                return false;                     
+            else
+                return f1.N < f2.N && f1.D < f2.D;
+
+        }
+
+    }
+}
---
+++ operatori frazioni/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Frazioni;
+
+namespace operatori_frazioni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction(1, 5);
+            Fraction f2 = new Fraction(2, 5);
+            Console.WriteLine(f1 <= f2);
+            Console.ReadLine();
+        }
+    }
+}
